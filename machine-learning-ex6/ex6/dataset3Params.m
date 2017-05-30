efde90a8cff9039e49397a9beeecdcd6087a6593
Libra Huang <librah@tw.ibm.com>
2017-05-31 00:08:40 +0800

    Check in ex6 programming assignment works
    Based on https://github.com/schneems/Octave/tree/master/mlclass-ex6/mlclass-ex6

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,28 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% comment out the following trial/errors
+%values = [0.01 0.03 0.1 0.3 1 3 10 30];
+%error_min = inf;
+%
+%for C = values
+%  for sigma = values
+%    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+%    err   = mean(double(svmPredict(model, Xval) ~= yval));
+%    if( err <= error_min )
+%      C_final     = C;
+%      sigma_final = sigma;
+%      error_min   = err;
+%    end
+%  end
+%end
+%C     = C_final;
+%sigma = sigma_final;
+%fprintf('Best value C, sigma = [%f %f] with prediction error = %f\n', C, sigma, error_min);
 
-
-
-
+% final values that give the best result / minimal errors
+C = 1.0;
+sigma = 0.1;
 
 
 % =========================================================================

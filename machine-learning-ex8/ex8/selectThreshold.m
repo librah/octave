5f52309e95bf25d4a6bfb02000781b2a62f540d6
Libra Huang <librah@tw.ibm.com>
2017-06-04 12:24:45 +0800

    Check in ex8 programming assignment works
    Based on https://github.com/benoitvallon/coursera-machine-learning/tree/master/machine-learning-ex8/ex8

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -23,17 +23,29 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    cvPredictions = (pval < epsilon);
 
+    tp = sum((cvPredictions == 1) & (yval == 1));
+    fp = sum((cvPredictions == 1) & (yval == 0));
+    fn = sum((cvPredictions == 0) & (yval == 1));
 
+    if tp + fp > 0
+      prec = tp / (tp + fp);
+    else
+      prec = 0;
+    end
 
+    if (tp + fn) > 0
+      rec = tp / (tp + fn);
+    else
+      rec = 0;
+    end
 
-
-
-
-
-
-
-
+    if prec + rec > 0
+      F1 = 2 * prec * rec / (prec + rec);
+    else
+      F1 = 0;
+    end
 
     % =============================================================
 
